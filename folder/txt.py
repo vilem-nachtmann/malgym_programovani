14853101fffb493ece6169255081426a0aa3575b
Vilém Nachtmann <56540691+vilem-nachtmann@users.noreply.github.com>
2020-11-20 09:14:56 +0100

    fred code

---
+++ folder/txt.py
@@ -0,0 +1,7 @@
+"""
+soubor = open("C:\\Users\\vilem\\OneDrive\\Desktop\\Škola\\Informatika\\txt\\soubor.txt", "r")
+for i in (soubor.readlines()): print(i)
+"""
+soubor2 = open("soubor2.txt", "w")
+soubor2.write("Ron\nFred\nJerry")
+soubor2.close()
